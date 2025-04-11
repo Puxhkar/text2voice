c11bdc90337f692c245c8a2142dd855adc5757cb
Pushkar Gupta <pushkargupta606@gmail.com>
2025-04-11 23:49:27 +0530

    Update convertor.js

--- convertor.js
+++ convertor.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+ document.addEventListener('DOMContentLoaded', function() {
     const text = document.getElementById("textToConvert");
     const convertBtn = document.getElementById("convertBtn");
 
